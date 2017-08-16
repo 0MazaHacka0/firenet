74ea50be43a5f8158baf6ec249f3a425f12f1128
0MazaHacka0 <killerinshadow2@gmail.com>
2017-08-17 00:55:35 +0300

    First fix!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {firebaseConfig} from './config.js';
+import {firebaseConfig} from './config.ts';
 
 @NgModule({
   declarations: [
