601e3a6c53534a0db06fc34966076ae8d7f7bed7
0MazaHacka0 <killerinshadow2@gmail.com>
2017-08-18 22:36:38 +0300

    Create login page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {firebaseConfig} from './config.ts';
+import {firebaseConfig} from './config';
+
+import {AF} from "./providers/af";
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [AF],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
