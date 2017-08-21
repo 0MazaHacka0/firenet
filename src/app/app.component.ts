dbb585593f43672d7c0bb6e1840f2602a925b1aa
Nick Gant <beltyukovdanil@gmail.com>
2017-08-22 00:46:00 +0300

    User data stores in db, add 'me' and 'user' components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { DOCUMENT } from '@angular/common';
 import { AuthService } from './providers/auth.service';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(public authService: AuthService, private router: Router) {
-    this.router.navigate(['']);
+  constructor(public authService: AuthService, private router: Router, db: AngularFireDatabase) {
+    // this.router.navigate(['']);
+    authService.afAuth.authState.subscribe(
+      (auth) => {
+        if (auth == null) {
+          console.log('Logged out');
+        } else {
+          console.log(auth);
+          console.log(`users/${auth.uid}`);
+          db.object(`users/${auth.uid}`).set(auth.providerData[0]);
+        }
+      }
+    );
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,26 +6,31 @@ import { AppComponent } from './app.component';
 import { firebaseConfig } from './config';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AuthService } from './providers/auth.service';
 import { HomePageComponent } from './home-page/home-page.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { UserPageComponent } from './user-page/user-page.component';
+import { MeComponent } from './me/me.component';
+import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'users', component: UserPageComponent },
+  { path: 'me', component: MeComponent }
 ];
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, NavbarComponent, UserPageComponent],
+  declarations: [AppComponent, HomePageComponent, NavbarComponent, UserPageComponent, MeComponent, UserComponent],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFireAuthModule,
+    AngularFireDatabaseModule,
     RouterModule.forRoot(routes)
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
